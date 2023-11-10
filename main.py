8eec59d0b7b7449a9a8e2f12e36f8d152156456d
norkos <norbert.kosmal@gmail.com>
2023-11-10 16:21:26 +0100

    Creating folder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import boto3
+from names_generator import generate_name
 from botocore.exceptions import NoCredentialsError
 
 
@@ -32,9 +33,22 @@ def download_from_aws(bucket, s3_file, local_file):
         return False
 
 
-local_file_name = 'work.jpg'
-bucket_name = 'norkos'
-s3_file_name = 'file_in_s3.jpg'
+def create_folder_in_s3(bucket, folder_name):
+    s3 = boto3.client('s3')
 
-upload_to_aws(local_file_name, bucket_name, s3_file_name)
-download_from_aws(bucket_name, s3_file_name, f"fetched_{local_file_name}")
+    folder_key = folder_name + '/'
+    try:
+        s3.put_object(Bucket=bucket, Key=folder_key)
+        print(f"Folder '{folder_name}' created in bucket '{bucket}'")
+    except NoCredentialsError:
+        print("Credentials not available")
+
+
+dir_name = generate_name()
+file_name = 'work.jpg'
+bucket_name = 'norkos'
+s3_file_name = f"{dir_name}/{file_name}"
+
+create_folder_in_s3(bucket_name, dir_name)
+upload_to_aws(file_name, bucket_name, s3_file_name)
+download_from_aws(bucket_name, s3_file_name, f"fetched_{file_name}")
